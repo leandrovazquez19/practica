a7c9470d60f5b365251bf242badd94b27d2bb58a
Leandro Vazquez <leandrovazquez2@outlook.com>
2022-05-09 18:01:04 -0300

    Prueba

---
+++ JavaScript/operaciones-aritmetica.js
@@ -0,0 +1,32 @@
+/*
+. suma = +
+. resta = - 
+. multiplicacion = *
+. division = / 
+. resto de una division = %
+
+
+' var suma = 3 + 2;
+! suma tendria una variable de 5.
+
+' var primerNumero = 5;
+' var segundoNumero = 2;
+' var resultado = primerNumero - segundoNumero;
+' var resultado = primerNumero + 3;
+' var primerNumero++;
+' var segundoNumero--;
+! Se pueden hacer operaciones con  variables.
+! ++ suma 1 al valor de la variable.
+! -- resta 1 al valor de la variable.
+
+' var resto = 15 % 7;
+! esta operacion dara como resultado 1 que es el resto de dividir 15 entre 7.
+
+' var a = 5;
+' a += 5;
+' a -= 5;
+' a *= 5:
+' a /= 5;
+! Tambien podemos sumar, restar, multiplicar o dividir de esta forma. Estamos sumando, restando, multiplicando o dividiendo a la variable a.
+
+*/
---
+++ JavaScript/variables.js
@@ -0,0 +1,71 @@
+/*
+% Tipos de Datos:
+
+. undefined= no existe un valor asignado
+. null= un objeto es nulo 
+. boolean= valores booleanos (verdadero/falso)
+. string= cadena de caracteres
+. symbol=
+. number= representa numeros
+. object= representa objetos 
+
+
+
+' console.log ();
+! muestra en la consola los valores que estan entre parentesis.
+
+
+' var "nombre" = ;
+' let "nombre" = ;
+' const "nombre" = ;
+! Crea una variable
+
+
+' var a;
+' a = 10;
+! "=" es un operador de asignacion. Con esto hacemos que la variable undefined "a" tenga el valor "10".
+
+
+% Valores de tipo Number, String, Boolean, Undefined:
+' valor 284 
+' valor "Leandro" 'Vazquez'
+' valor true / false ( "!" indica negación, !true = false )
+' valor 
+' valor variable
+! Tambien se pueden usar variables para asignar valores.
+
+
+% Secuencas de Escape:
+. Codigo  |   Resultado
+.------------------------
+' \'      |   Comilla Simple      
+' \"      |   Comilla Doble
+' \\      |   Barra Invertida
+' \n      |   Linea NUeva
+' \r      |   Retorno de Carro
+' \t      |   Tabulacion
+' \b      |   Retroceso
+' \f      |   Salto de Pagina
+! Son codigos que se escriben dentro de un string para hacer distintas cosas.
+
+
+% Longitud de una Cadena de Caracteres:
+' var cadenaDeCaracteres;
+' cadenaDeCaracteres = "Hola";
+' console.log (cadenaDeCaracteres.length);
+! .length cuenta cuandos caracteres contienen el string contando los espacios que hay entre cada palabra.
+
+
+% Notacion de Corchetes:
+' var variable = "JavaScript";
+. Cadena:   J a v a S c r i p t
+. Indices:  0 1 2 3 4 5 6 7 8 9
+' console.log (variable[3]);
+! Esto nos mostrara la posicion numero 3 de los caracteres de JavaScript.
+
+
+
+
+
+
+*/
