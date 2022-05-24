48133af5712c04b6c3f305e93568479a8d4f96b3
Leandro Vazquez <leandrovazquez2@outlook.com>
2022-05-23 22:22:12 -0300

    Prueba

--- JavaScript/interactuar_con_pagina_web.js
+++ JavaScript/interactuar_con_pagina_web.js
@@ -43,3 +43,38 @@ createElemento1.innerHTML = "Texto";
 
 bodyElemento.removeChild(createElemento1);
 Haciendo esto lo que hago es eliminar el elemento p que cree con removeChild(NombreDeLaVariable).
+
+
+Obtencon de la entrada del usuario:
+
+var entrada = document.getElementById("ID").value;
+De esta manera se obtiene la entrada del usuario.
+
+
+Obtencion de un elemento segun su posicion:
+
+var bodyChildren = document.body.children;
+console.log(bodyChildren[1].innerHTML = "Modificacion");
+De esta manera se trae un elemento hijo de el elemento padre "Body" y modificarlo.
+
+var bodyChildren = document.body.firstElementChild;
+console.log(bodyChildren);
+Aca con firstElementChild se accede al primer elemento hijo del elemento padre "Body".
+
+var elementSibling = document.head.nextElementSibling;
+console.log(elementSibling);
+De esta manera se esta accediendo al primer elemento hermano mas cercano que tiene "Head".
+var elementSibling = document.body.firstChild.nextElementSibling;
+console.log(elementSibling);
+De esta manera se esta accediendo al primer elemento hermano mas cercano que tiene "Body" y despues selecciona al elemento hermano mas cercano.
+
+
+Acceder y cambiar elementos especificos:
+
+var variable = document.querySelector("Elemento");
+var variable = document.querySelector("#ID");
+var variable = document.querySelector(".CLASE");
+var variable = document.querySelector(".clase1.clase2");
+var variable = document.querySelector("p.ID");
+variable.innerHTML = "Modificacion";
+De esta forma recupero elementos HTML individuales y los modifico. Solo recupera el primer elemento que encuentra, si hay 2 elementos con la misma etiqueta o clase, solo accede al primero.
